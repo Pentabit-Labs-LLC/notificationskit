9088666896b3126b0b2ab608d4e1543b2e6ec6a1
muhammadtalha <talha.c@pentabitlabs.com>
2026-09-07 18:18:51 +0500

    Release 0.2.1.0 (Android AAR + common multiplatform)

--- notificationskit/build.gradle.kts
+++ notificationskit/build.gradle.kts
@@ -27,8 +27,8 @@ publishing {
         create<MavenPublication>("NotificationsKit") {
             groupId = "com.github.Pentabit-Labs-LLC"
             artifactId = "notificationskit"
-            version = "0.2.0.0"
-            artifact("$projectDir/libs/NotificationsKit_v0200.aar")
+            version = "0.2.1.0"
+            artifact("$projectDir/libs/NotificationsKit_v0210.aar")
         }
     }
 }
